f30d3bedc9bb134de36946528e3c177819ae07af
Taiki Ono <taiks.4559@gmail.com>
2015-05-27 10:25:46 +0900

    Spec for MixedGauge::Model

---
+++ spec/mixed_gauge/model_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+RSpec.describe MixedGauge::Model do
+  let!(:model) do
+    Class.new(ActiveRecord::Base) do
+      def self.name
+        'User'
+      end
+
+      include MixedGauge::Model
+      use_cluster :user
+      def_distkey :name
+    end
+  end
+
+  let(:user_attributes) { { name: 'Alice', email: 'alice@example.com' } }
+
+  describe '.put!' do
+    it 'creates new record into proper node' do
+      record = model.put!(user_attributes)
+      expect(record).to be_a(model)
+      expect(record.name).to eq('Alice')
+      expect(record).to be_respond_to(:save!)
+    end
+  end
+
+  describe '.get' do
+    before { model.put!(user_attributes) }
+
+    it 'gets AR::B instance from proper node' do
+      record = model.get('Alice')
+      expect(record).to be_a(model)
+      expect(record.name).to eq('Alice')
+      expect(record).to be_respond_to(:save!)
+    end
+  end
+
+  describe '.shard_for' do
+    before { model.put!(user_attributes) }
+
+    it 'enables to use finder method' do
+      record = model.shard_for('Alice').find_by(email: 'alice@example.com')
+      expect(record).not_to be_nil
+      expect(record.name).to eq('Alice')
+    end
+  end
+
+  describe '.all_shards' do
+    before { model.put!(user_attributes) }
+
+    it 'returns all AR model classes and can search by finder methods' do
+      records = model.all_shards.flat_map {|m| m.find_by(email: 'alice@example.com') }.compact
+      expect(records.size).to eq(1)
+    end
+  end
+end
