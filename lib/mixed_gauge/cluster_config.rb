a7fe869ed43e4671356b85a7c49e458ba73191e6
Taiki Ono <taiks.4559@gmail.com>
2015-06-11 20:40:05 +0900

    Validate cluster configuration

--- lib/mixed_gauge/cluster_config.rb
+++ lib/mixed_gauge/cluster_config.rb
@@ -21,9 +21,9 @@ module MixedGauge
       @connection_registry[assigned_slots] = connection
     end
 
+    # @raise [RuntimeError]
     def validate_config!
-      # TODO
-      # validate non Fixnum slots.
+      Validator.new(slot_size, @connection_registry).validate!
     end
 
     # @return [Integer]
@@ -40,6 +40,79 @@ module MixedGauge
     # @return [Array<Symbol>] An array of connection name
     def connections
       @connection_registry.values
+    end
+
+    class Validator
+      # @param [Integer] slot_size
+      # @param [Hash{Range => Symbol}] connection_registry
+      def initialize(slot_size, connection_registry)
+        @slot_size = slot_size
+        @connection_registry = connection_registry
+      end
+
+      # @raise [RuntimeError]
+      def validate!
+        all_start_points = @connection_registry.keys.map(&:first).sort
+        all_end_points = @connection_registry.keys.map(&:last).sort
+
+        check_first_start_point(all_start_points.first)
+        check_coverage(all_start_points, all_end_points)
+        check_last_end_point(all_end_points.last)
+      end
+
+      private
+
+      # @param [Integer] first_start_point
+      def check_first_start_point(first_start_point)
+        unless first_start_point == 0
+          report_invalid_first_start_point(first_start_point)
+        end
+      end
+
+      # @param [Array<Integer>] all_start_points
+      # @param [Array<Integer>] all_end_points
+      def check_coverage(all_start_points, all_end_points)
+        all_end_points.each_with_index do |end_point, i|
+          break if all_end_points.size == i + 1
+
+          next_start_point = all_start_points[i + 1]
+          unless end_point.succ == next_start_point
+            report_invalid_coverage(end_point, all_start_points[i + 1])
+          end
+        end
+      end
+
+      # @param [Integer] last_end_point
+      def check_last_end_point(last_end_point)
+        unless last_end_point == @slot_size - 1
+          report_invalid_last_end_point(last_end_point)
+        end
+      end
+
+      # @param [Integer] point
+      def report_invalid_first_start_point(point)
+        r = @connection_registry.keys.find {|range| range.first == point }
+        connection = @connection_registry[r]
+        raise "First start point must be `0` but given `#{point}`: invalid slot configuration for #{connection}"
+      end
+
+      # @param [Integer] end_point
+      # @param [Integer] next_start_point
+      def report_invalid_coverage(end_point, next_start_point)
+        end_point_slot = @connection_registry.keys.find {|range| range.last == end_point }
+        end_point_connection = @connection_registry[end_point_slot]
+        start_point_slot = @connection_registry.keys.
+          find {|range| range.first == next_start_point && range.last != end_point }
+        start_point_connection = @connection_registry[start_point_slot]
+        raise %!End point `#{end_point}` of "#{end_point_connection}" or start point `#{next_start_point}` of "#{start_point_connection}" is invalid. Next start point must be "previous end point + 1".!
+      end
+
+      # @param [Integer] point
+      def report_invalid_last_end_point(point)
+        r = @connection_registry.keys.find {|range| range.last == point }
+        connection = @connection_registry[r]
+        raise "Last end point must be `#{@slot_size - 1}` but given `#{point}`: invalid slot configuration for #{connection}"
+      end
     end
   end
 end
--- lib/mixed_gauge/config.rb
+++ lib/mixed_gauge/config.rb
@@ -15,9 +15,11 @@ module MixedGauge
     # See README.md for example.
     # @param [Symbol] cluster_name
     # @yield [MixedGauge::ClusterConfig]
+    # @raise [RuntimeError] When this cluster config is invalid.
     def define_cluster(cluster_name, &block)
       cluster_config = ClusterConfig.new(cluster_name)
       cluster_config.instance_eval(&block)
+      cluster_config.validate_config!
       @cluster_configs[cluster_name] = cluster_config
     end
 
